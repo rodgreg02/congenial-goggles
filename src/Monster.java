d4e0448873b68017036d70da52ff656a24e4ff67
rodgreg02 <mindera@ME00391.local>
2023-11-21 16:29:05 +0000

    minor fix

--- src/Monster.java
+++ src/Monster.java
@@ -1,14 +1,11 @@
 public abstract class Monster {
     int healthPoints;
     int attackPoints;
-    boolean alive;
+
 
     abstract public int attack();
     public int getHit(int attackPoints){
         this.healthPoints = this.healthPoints - attackPoints;
-        if(this.healthPoints <= 0){
-            this.alive = false;
-        }
         return this.healthPoints;
     }
 }
